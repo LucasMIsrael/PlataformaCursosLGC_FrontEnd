e3bb090eaf6385bc42d7fda46bce44ca5df25181
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-05-18 20:54:36 -0300

    02 - configuração de cores e componentes; criação do Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
-import styles from "../styles/HomeNoAuth.module.scss";
+import styles from "../styles/homeNoAuth.module.scss"
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = function () {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>SkillUp – Plataforma de Cursos</title>
+        <link rel="shortcut icon" href="/logoCurso.png" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
