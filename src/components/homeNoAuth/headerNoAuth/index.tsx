e978d15c88b33333710f260222b88ab565f30f25
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-05-18 22:18:20 -0300

    03 - criação da seção e edição de imagens

--- src/components/homeNoAuth/headerNoAuth/index.tsx
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -1,24 +1,30 @@
-import { Button, Container } from "reactstrap";
-import styles from "./styles.module.scss";
 import Link from "next/link";
+import { Container, Button } from "reactstrap";
+import styles from "./styles.module.scss";
 
 const HeaderNoAuth = function () {
     return (
         <>
-            <div className={styles.ctaSection}>
-                <p>Se cadastre para ter acesso aos cursos</p>
-            </div>
-            <Container className={styles.nav}>
-                <img src="/logoCurso.png" alt="logoOnebitflix" className={styles.imgLogoNav} />
-                <div>
-                    <Link href="/login">
-                        <Button className={styles.navBtn} outline>Entrar</Button>
-                    </Link>
-                    <Link href="/register">
-                        <Button className={styles.navBtn} outline>Quero fazer parte</Button>
-                    </Link>
+            <>
+                <div className={styles.ctaSection}>
+                    <p>Se cadastre para ter acesso aos cursos</p>
                 </div>
-            </Container>
+                <Container className={styles.nav}>
+                    <img
+                        src="/logoCurso.png"
+                        alt="logocurso"
+                        className={styles.imgLogoNav}
+                    />
+                    <div>
+                        <Link href="/login">
+                            <Button className={styles.navBtn}>Entrar</Button>
+                        </Link>
+                        <Link href="/register">
+                            <Button className={styles.navBtn}>Quero fazer parte</Button>
+                        </Link>
+                    </div>
+                </Container>
+            </>
         </>
     );
 };
