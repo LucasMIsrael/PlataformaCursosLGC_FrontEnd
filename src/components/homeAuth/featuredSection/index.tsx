4216faf50421e24bca5f8ae63c6594af6773cf9b
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-05-29 20:19:45 -0300

    17 - continuação da seção de cursos em destaque (usuario logado)

--- src/components/homeAuth/featuredSection/index.tsx
+++ src/components/homeAuth/featuredSection/index.tsx
@@ -19,9 +19,15 @@ const FeaturedSection = function () {
     <>
       {
         data.data?.map((course: CourseType) => (
-          <div>
+          <div style={{
+            backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "600px",
+          }}
+            key={course.id}>
             <HeaderAuth />
-            <Container>
+            <Container className="pt-4">
               <p className={styles.title}>{course.name}</p>
               <p className={styles.description}>{course.synopsis}</p>
               <Link href={`/courses/${course.id}`}>
